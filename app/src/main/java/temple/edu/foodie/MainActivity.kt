f702b54332ac57b72935d7a30092a84ff8217876
BrockMorris42611 <brock36morris@gmail.com>
2022-04-21 04:04:18 -0400

    User Registration has been made and makes call to the registeringAPI. Right now there is an issue where if the registeringAPI is ran locally, it works with no issues at all.  BUT once it is accessed from the server or the application itself, it starts to produce a 400 error. the 400 error only seems to be happening when I supply a valid JSON object. It is confusing since it works locally. I have also worked on getting Volley set up and getting restaurants back from the DB but I still need to have them populate the recycler view. It is also that I just need to set up the map which shouldnt be hard and then i can put in pins since the DB hold LATLONGS.
    -Brock Morris

--- app/src/main/java/temple/edu/foodie/MainActivity.kt
+++ app/src/main/java/temple/edu/foodie/MainActivity.kt
@@ -25,24 +25,26 @@ class MainActivity : AppCompatActivity() {
         val navView: BottomNavigationView = findViewById(R.id.bottomNavigationViewMenu)
 
         //val navController = findNavController(R.id.main)
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
         val navController = navHostFragment.navController
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
-            setOf(R.id.loginFrag, R.id.registerFrag, R.id.searchFrag, R.id.listFrag))
+            setOf(R.id.loginFrag, R.id.registerFrag, R.id.searchFrag, R.id.listFrag)
+        )
 
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
-        navController.addOnDestinationChangedListener{_, _, _ ->
-            if(navController.currentDestination?.id == R.id.loginFrag || navController.currentDestination?.id == R.id.registerFrag){
+        navController.addOnDestinationChangedListener { _, _, _ ->
+            if (navController.currentDestination?.id == R.id.loginFrag || navController.currentDestination?.id == R.id.registerFrag) {
                 navView.visibility = View.GONE
-            }else{
+            } else {
                 navView.visibility = View.VISIBLE
             }
         }
         navView.findViewById<View>(R.id.action_search).setOnClickListener {
-                navController.navigate(R.id.searchFrag)
+            navController.navigate(R.id.searchFrag)
         }
         navView.findViewById<View>(R.id.action_list).setOnClickListener {
             navController.navigate(R.id.listFrag)
@@ -51,7 +53,8 @@ class MainActivity : AppCompatActivity() {
             navController.navigate(R.id.mapsFrag)
         }
 
-        val url = "http://cis-linux2.temple.edu:8080/SP22_4515_tuj42611/getRestaurantId.jsp?restaurantId=0"
+        val url =
+            "http://cis-linux2.temple.edu:8080/SP22_4515_tuj42611/getRestaurantId.jsp?restaurantId=20"
         val requestQueue = Volley.newRequestQueue(this)
         val obj = JSONObject()
 
@@ -61,17 +64,25 @@ class MainActivity : AppCompatActivity() {
             obj,
             object : Response.Listener<JSONObject> {
                 override fun onResponse(response: JSONObject?) {
-                    try{
+                    try {
                         Log.d(">>>>>>>>>>>>>>>>>>>>", response.toString())
-                        Log.d("********************", ""/*response.toString()*/)
-                    }catch (e: JSONException){
+                        Log.d("********************", obj.toString()/*response.toString()*/)
+                    } catch (e: JSONException) {
                         System.out.println(">>>>>>>>>>>>>>>>>>>>XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
                     }
                 }
             },
             Response.ErrorListener {
-                System.out.println(">>>>>>>>>>>>>>>>>>>>DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD" + it.toString())
+                println(">>>>>>>>>>>>>>>>>>>>DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD$it")
             })
         requestQueue.add(jsonObjectRequest)
+
+        val newObj = JSONObject()
+        newObj.put("x", 11)
+        
+
+        println(">>>>>>>>>>>>>>>>>>>>DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD$newObj")
     }
 }
+
+class test (x : Int)
--- app/src/main/java/temple/edu/foodie/RegisterFrag.kt
+++ app/src/main/java/temple/edu/foodie/RegisterFrag.kt
@@ -1,40 +1,85 @@
 package temple.edu.foodie
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONException
+import org.json.JSONObject
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [RegisterFrag.newInstance] factory method to
- * create an instance of this fragment.
- */
 class RegisterFrag : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    lateinit var email : TextView
+    lateinit var username : TextView
+    lateinit var password : TextView
+    lateinit var password2 : TextView
+    lateinit var registerButton: Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
-    }
+        val layout = inflater.inflate(R.layout.fragment_register, container, false)
 
+        email = layout.findViewById(R.id.emailTV)
+        username = layout.findViewById(R.id.usernameTV)
+        password = layout.findViewById(R.id.passwordTV)
+        password2 = layout.findViewById(R.id.password2TV)
+        registerButton = layout.findViewById(R.id.registerButton)
+
+        email.error
+
+        registerButton.setOnClickListener {
+            if      (email.text.isNullOrEmpty() || !email.text.contains("@"))
+                email.error = "Please enter a valid email"
+            else if (username.text.isNullOrEmpty())
+                username.error = "Please enter a username"
+            else if (password.text.isNullOrEmpty())
+                password.error = "Please enter a password"
+            else if (password2.text.isNullOrEmpty())
+                password2.error = "Please re-type your password"
+            else if (!password.text.toString().equals(password2.text.toString(), false))
+                Toast.makeText(this.requireContext(), "Please make sure the two passwords match", Toast.LENGTH_LONG).show()
+            else{
+                val toReg = JSONObject()
+                toReg.put("email"   , email.text.toString())
+                toReg.put("username", username.text.toString())
+                toReg.put("password", password.text.toString())
+
+                val requestQueue = Volley.newRequestQueue(this.requireContext())
+                val obj = JSONObject()
+                val url =
+                    "http://cis-linux2.temple.edu:8080/SP22_4515_tuj42611/" + "insertUser.jsp" + "?jsonData=" + toReg
+                val jsonObjectRequest = JsonObjectRequest(
+                    Request.Method.GET,
+                    url,
+                    obj,
+                    object : Response.Listener<JSONObject> {
+                        override fun onResponse(response: JSONObject?) {
+                            try {
+                                Log.d(">>>>>>>>>>>>>>>>>>>>", response.toString())
+                                Log.d("********************", obj.toString()/*response.toString()*/)
+                            } catch (e: JSONException) {
+                                System.out.println(">>>>>>>>>>>>>>>>>>>>XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+                            }
+                        }
+                    },
+                    Response.ErrorListener {
+                        println(">>>>>>>>>>>>>>>>>>>>DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD$it")
+                    })
+                requestQueue.add(jsonObjectRequest)
+            }
+        }
+
+        return layout
+    }
 }
